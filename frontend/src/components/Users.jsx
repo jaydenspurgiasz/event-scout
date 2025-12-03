4971184db98c59827e8f895968bf99363fde8790
JH22P <jp26@g.ucla.edu>
2025-12-03 13:01:21 -0800

    added regex to the register so that we can check if the user's input is valid

---
+++ frontend/src/components/Users.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+export default function FriendSearch() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [users, setUsers] = useState([]);
+
+  
+}
